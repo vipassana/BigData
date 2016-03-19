efecf4ffbd458a1f6e8f7c4ae5f5034c3eecbe53
vipassana <vipassana.me@gmail.com>
2016-03-18 23:22:20 -0400

    Adding Lab6

---
+++ Lab6/map.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import sys
+
+
+#input comes from STDIN (stream data that goes to the program)
+for line in sys.stdin:
+   
+	#Fill in your map code here. To write to output file, use "print" 
---
+++ Lab6/reduce.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+import sys
+
+
+#input comes from STDIN (stream data that goes to the program)
+for line in sys.stdin:
+
+	#Fill in your reduce code here. To write to output file, use "print"
+
