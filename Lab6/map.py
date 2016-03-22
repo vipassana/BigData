8979880b8e1e1b9c31a81140c4ea99121b517a38
vipassana <vipassana.me@gmail.com>
2016-03-21 23:32:10 -0400

    sompleted map

--- Lab6/map.py
+++ Lab6/map.py
@@ -1,9 +1,16 @@
+
+
 #!/usr/bin/python
 
 import sys
 
 
 #input comes from STDIN (stream data that goes to the program)
-for line in sys.stdin:
-   
-	#Fill in your map code here. To write to output file, use "print" 
+f = open('professors.txt', 'r')
+for line in f.readlines():
+
+	#Fill in your map code here. To write to output file, use "print"
+    l = line.strip().split(',')
+    print l
+
+f.close()
