1a70cfd5c6cf8c0fe569561a8f295e943bb2cec8
vipassana <vipassana.me@gmail.com>
2016-04-04 18:27:21 -0400

    Adding trip and fare info for task 1

--- Assignment2/scripts/task1/map.py
+++ Assignment2/scripts/task1/map.py
@@ -2,14 +2,16 @@
 
 import sys
 import os
+import re
 
 for line in sys.stdin:
 
     line = line.strip().split(',')
-
+    ip_file = os.environ['mapreduce_map_input_file'].strip('.csv')
+    ip_file = re.sub('hdfs.*/','',ip_file)
     if len(line) == 11: # fare
         medallion, hack_license, vendor_id, pickup_datetime, payment_type, fare_amount, surcharge, mta_tax, tip_amount, tolls_amount, total_amount = line
-        print ('%s,%s,%s,%s,%s&%s,%s,%s,%s,%s,%s,%s' % (medallion, hack_license, vendor_id, pickup_datetime,os.environ['mapreduce_map_input_file'], payment_type, fare_amount, surcharge, mta_tax, tip_amount, tolls_amount, total_amount))
+        print ('%s,%s,%s,%s&%s,%s,%s,%s,%s,%s,%s,%s' % (medallion, hack_license, vendor_id, pickup_datetime, payment_type, fare_amount, surcharge, mta_tax, tip_amount, tolls_amount, total_amount, ip_file))
     elif len(line) == 14: # trip
         medallion, hack_license, vendor_id, rate_code, store_and_fwd_flag, pickup_datetime, dropoff_datetime, passenger_count, trip_time_in_secs, trip_distance, pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude = line
-        print ('%s,%s,%s,%s,%s&%s,%s,%s,%s,%s,%s,%s,%s,%s,%s' % (medallion, hack_license, vendor_id, pickup_datetime,os.environ['mapreduce_map_input_file'], rate_code, store_and_fwd_flag, dropoff_datetime, passenger_count, trip_time_in_secs, trip_distance, pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude))
+        print ('%s,%s,%s,%s&%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s' % (medallion, hack_license, vendor_id, pickup_datetime, rate_code, store_and_fwd_flag, dropoff_datetime, passenger_count, trip_time_in_secs, trip_distance, pickup_longitude, pickup_latitude, dropoff_longitude, dropoff_latitude, ip_file))
--- Assignment2/scripts/task1/reduce.py
+++ Assignment2/scripts/task1/reduce.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python
 
 import sys
-
 cnt = 0
 
 for line in sys.stdin:
 
     k, v = line.strip().split('&')
-    cnt += 1
-
-    if cnt % 2 == 1: # trip - 1st
-        trip_v = v
-    else: # fare
-        if k != "medallion,hack_license,vendor_id,pickup_datetime":
-            print ("%s	%s" % (k, (trip_v + ',' + v)))
+    if k != "medallion,hack_license,vendor_id,pickup_datetime":
+        if 'trips' in v:
+            trip_v = v
+        else: # fare
+            if k != "medallion,hack_license,vendor_id,pickup_datetime":
+                trip_v = trip_v.strip(',trips_sunday')
+                trip_v = trip_v.strip(',trips_wednesday')
+                v = v.strip(',fares_sunday')
+                v = v.strip(',fares_wednesday')
+                print ("%s%s" % (k, (trip_v + ',' + v)))
